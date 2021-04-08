c89da2d1f315fa8928b53877a036841f7751e66c
Krzysztof <pozixxx@protonmail.com>
2021-04-08 20:46:36 +0200

    Updates

---
+++ precache-manifest.e88281dff7c8610f4e7c2c596186900d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b2663200dbba9eb96f2",
+    "url": "/app-react-router/static/css/main.963f5236.chunk.css"
+  },
+  {
+    "revision": "2b2663200dbba9eb96f2",
+    "url": "/app-react-router/static/js/main.4fc92a9b.chunk.js"
+  },
+  {
+    "revision": "d466ce07c2972432d57f",
+    "url": "/app-react-router/static/js/runtime~main.6a4469d8.js"
+  },
+  {
+    "revision": "d62121c24aa66bcb8dfa",
+    "url": "/app-react-router/static/js/2.682986c1.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-react-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "34ff2eb06b91366a696d3c519b42b1ba",
+    "url": "/app-react-router/index.html"
+  }
+];
